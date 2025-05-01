7ebf443ba08839b6a41b42570d8198b548aa55d5
Qasim Shahid <qshahid@gmu.edu>
2025-05-01 16:33:32 -0400

    updates

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def delete_survey(id: int):
 def get_version_info():
     return HTMLResponse(
         content="""
-        <html><head><title>Survey API</title></head><body>
+        <html><head><title>Survey API Extra Credit Python Version</title></head><body>
         <h1>Survey API Version</h1>
         <p>Version: 3.0.0.0</p>
         <p>Author: Qasim Shahid - SWE 645</p>
